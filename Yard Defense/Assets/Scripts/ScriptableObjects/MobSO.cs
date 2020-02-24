5ce32cc577afce7b4915d0e59b44350e1a393021
Erik <ErikOverflow@gmail.com>
2020-02-23 23:25:30 -0600

    Add butterfly scriptableObject

---
+++ Yard Defense/Assets/Scripts/ScriptableObjects/MobSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YardDefense.Mob
+{
+    [CreateAssetMenu(fileName = "New mob", menuName = "Create new mob", order = 1)]
+    public class MobSO : SpriteableObject
+    {
+        public int health = 10;
+        public int attackDamage = 1;
+        public float attackFrequency = 0.5f;
+    }
+}
