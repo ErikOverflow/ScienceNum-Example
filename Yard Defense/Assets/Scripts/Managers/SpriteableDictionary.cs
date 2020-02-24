0e317191fd6a83c7c7cd4ee7bde5690653debaef
Erik <ErikOverflow@gmail.com>
2020-02-23 23:25:10 -0600

    Add SpriteableObjects

---
+++ Yard Defense/Assets/Scripts/Managers/SpriteableDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YardDefense
+{
+    public class SpriteableDictionary : MonoBehaviour
+    {
+        public static SpriteableDictionary Instance;
+        [SerializeField] SpriteableObject[] spriteableObjects;
+        Dictionary<string, Sprite> spriteDictionary;
+        private void Awake()
+        {
+            Instance = this;
+            spriteDictionary = new Dictionary<string, Sprite>();
+            spriteableObjects = Resources.FindObjectsOfTypeAll<SpriteableObject>();
+            foreach (SpriteableObject so in spriteableObjects)
+            {
+                spriteDictionary.Add(so.name, so.sprite);
+            }
+        }
+
+        public Sprite GetSprite(string name)
+        {
+            Sprite itemSprite;
+            spriteDictionary.TryGetValue(name, out itemSprite);
+            return itemSprite;
+        }
+    }
+}
---
+++ Yard Defense/Assets/Scripts/ScriptableObjects/SpriteableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YardDefense
+{
+    public abstract class SpriteableObject : ScriptableObject
+    {
+        [Header("Graphics and Animations")]
+        public Sprite sprite;
+    }
+}
